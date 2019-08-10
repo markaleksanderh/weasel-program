f66657bcff058d393e66857f1531313dbc80bb97
Mark Hil <mark@100shapes.com>
2019-08-10 17:23:23 +0100

    Return number

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import {List} from 'semantic-ui-react'
 
 import './App.css';
 
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      hits: [],
+      number: 0,
       isLoading: false,
     }
   }
@@ -15,23 +16,51 @@ class App extends Component {
     this.setState({isLoading:true})
     fetch('http://localhost:5000')
       .then(response => response.json())
-      .then(data => this.setState({hits: data, isLoading: false}))
+      .then(data => this.setState({number: data, isLoading: false}))
   }
   
   render() {
-    const {hits, isLoading} = this.state
+    const {number, isLoading} = this.state
     if (isLoading) {
       return <p>Loading...</p>
     }
     return (
-      <List>
-        {hits.map(hit =>
-        <List.Item>{hit}</List.Item>
-          
-          )}
-      </List>
+      <p>{number}</p>
     )
   }
 }
+
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       hits: [],
+//       isLoading: false,
+//     }
+//   }
+//   componentDidMount() {
+//     this.setState({isLoading:true})
+//     fetch('http://localhost:5000')
+//       .then(response => response.json())
+//       .then(data => this.setState({hits: data, isLoading: false}))
+//   }
+  
+//   render() {
+//     const {hits, isLoading} = this.state
+//     if (isLoading) {
+//       return <p>Loading...</p>
+//     }
+//     return (
+//       <List>
+//         {hits.map(hit =>
+//         <List.Item>{hit}</List.Item>
+          
+//           )}
+//       </List>
+//     )
+//   }
+// }
 
 export default App;
--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from weasel import weasel
+from random import randint
 import json
 
 def create_app():
@@ -8,8 +9,10 @@ def create_app():
     CORS(app)
     @app.route('/')
     def index():
-        output = weasel("hello world", 5, 5)
-        return json.dumps(output)
+        number = randint(0,100)
+        # output = weasel("hello world", 5, 5)
+        # return json.dumps(output)
+        return json.dumps(number)
 
     @app.errorhandler(404)
     def page_not_found(error):
